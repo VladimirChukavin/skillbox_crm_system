859b538f1b67441595f292d9aba340861818e62b
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 15:31:09 +1000

    Добавил код для модели.

--- crm_system/apps/customers/models.py
+++ crm_system/apps/customers/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+    lead = models.OneToOneField(
+        "Lead",
+        on_delete=models.CASCADE,
+        related_name="customer",
+        verbose_name="Потенциальный клиент",
+    )
+    contract = models.ForeignKey(
+        "Contract",
+        on_delete=models.CASCADE,
+        related_name="customers",
+        verbose_name="Контракт",
+    )
+
+    class Meta:
+        verbose_name = "Активный клиент"
+        verbose_name_plural = "Активные клиенты"
+        ordering = ("lead__full_name",)
+
+    def __str__(self):
+        return f"Активный клиент: {self.lead.full_name}"
