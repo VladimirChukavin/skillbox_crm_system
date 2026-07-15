93649bb361a8e39b1d351b17187a631c457b1a9a
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 15:29:28 +1000

    Добавил код для админки.

--- crm_system/apps/ads/admin.py
+++ crm_system/apps/ads/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import AdCampaign
+
+
+@admin.register(AdCampaign)
+class AdCampaignAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "product", "channel", "budget")
+    list_display_links = ("id", "name")
+    search_fields = ("id", "name", "channel", "budget")
