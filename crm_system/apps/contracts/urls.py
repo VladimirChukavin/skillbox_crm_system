9f7edd993b0420a0888e8e42c58e6499328e002b
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 16:31:26 +1000

    Создал файл urls.py.

---
+++ crm_system/apps/contracts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import (
+    ContractListView,
+    ContractCreateView,
+    ContractDetailView,
+    ContractUpdateView,
+    ContractDeleteView,
+)
+
+app_name = "contracts"
+
+urlpatterns = [
+    path("", ContractListView.as_view(), name="contracts-list"),
+    path("create/", ContractCreateView.as_view(), name="contracts-create"),
+    path("<int:pk>/", ContractDetailView.as_view(), name="contracts-detail"),
+    path("<int:pk>/edit/", ContractUpdateView.as_view(), name="contracts-edit"),
+    path("<int:pk>/delete/", ContractDeleteView.as_view(), name="contracts-delete"),
+]
