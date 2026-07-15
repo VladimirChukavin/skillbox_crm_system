db6d4f4cf93d445b772646302503b81521d42cb4
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 16:31:19 +1000

    Создал файл urls.py.

---
+++ crm_system/apps/ads/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+from .views import (
+    AdCampaignListView,
+    AdCampaignCreateView,
+    AdCampaignDetailView,
+    AdCampaignUpdateView,
+    AdCampaignDeleteView,
+    AdCampaignStatisticView,
+)
+
+app_name = "ads"
+
+urlpatterns = [
+    path("", AdCampaignListView.as_view(), name="ads-list"),
+    path("create/", AdCampaignCreateView.as_view(), name="ads-create"),
+    path("<int:pk>/", AdCampaignDetailView.as_view(), name="ads-detail"),
+    path("<int:pk>/edit/", AdCampaignUpdateView.as_view(), name="ads-edit"),
+    path("<int:pk>/delete/", AdCampaignDeleteView.as_view(), name="ads-delete"),
+    path("statistic/", AdCampaignStatisticView.as_view(), name="ads-statistic"),
+]
