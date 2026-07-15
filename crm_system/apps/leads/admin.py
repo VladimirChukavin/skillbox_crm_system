72e75f064fac4ea131aeb19cc3cb5a0a92bcef06
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 15:29:58 +1000

    Добавил код для админки.

--- crm_system/apps/leads/admin.py
+++ crm_system/apps/leads/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Lead
+
+
+@admin.register(Lead)
+class LeadAdmin(admin.ModelAdmin):
+    list_display = ("id", "full_name", "phone", "email", "ad_campaign")
+    list_display_links = ("full_name",)
+    search_fields = ("full_name", "phone", "email")
+    list_filter = ("ad_campaign",)
