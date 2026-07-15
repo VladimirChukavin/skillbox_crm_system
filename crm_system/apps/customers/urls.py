f76bb7552bd90e21eaa28ef49894be9e06bb95d5
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 16:31:32 +1000

    Создал файл urls.py.

---
+++ crm_system/apps/customers/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import (
+    CustomerListView,
+    CustomerCreateView,
+    CustomerDetailView,
+    CustomerUpdateView,
+    CustomerDeleteView,
+)
+
+app_name = "customers"
+
+urlpatterns = [
+    path("", CustomerListView.as_view(), name="customers-list"),
+    path("create/", CustomerCreateView.as_view(), name="customers-create"),
+    path("<int:pk>/", CustomerDetailView.as_view(), name="customers-detail"),
+    path("<int:pk>/edit/", CustomerUpdateView.as_view(), name="customers-edit"),
+    path("<int:pk>/delete/", CustomerDeleteView.as_view(), name="customers-delete"),
+]
