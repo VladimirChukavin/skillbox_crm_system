a21a2b015629cd5bbc737f293609f399aa7f1b53
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 16:31:41 +1000

    Создал файл urls.py.

---
+++ crm_system/apps/products/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import (
+    ProductListView,
+    ProductCreateView,
+    ProductDetailView,
+    ProductUpdateView,
+    ProductDeleteView,
+)
+
+app_name = "products"
+
+urlpatterns = [
+    path("", ProductListView.as_view(), name="products-list"),
+    path("create/", ProductCreateView.as_view(), name="products-create"),
+    path("<int:pk>/", ProductDetailView.as_view(), name="products-detail"),
+    path("<int:pk>/edit/", ProductUpdateView.as_view(), name="products-edit"),
+    path("<int:pk>/delete/", ProductDeleteView.as_view(), name="products-delete"),
+]
