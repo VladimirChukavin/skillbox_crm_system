9e9d427c3d228b59723bc013441151b555f4a9e6
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 15:31:28 +1000

    Добавил код для модели.

--- crm_system/apps/products/models.py
+++ crm_system/apps/products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    description = models.TextField(blank=True, default="", verbose_name="Описание")
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, verbose_name="Стоимость"
+    )
+
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
