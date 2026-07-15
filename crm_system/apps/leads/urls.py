6b5d072aac3652774f16f0f52e44951ed5b3123b
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 16:31:37 +1000

    Создал файл urls.py.

---
+++ crm_system/apps/leads/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from .views import (
+    LeadListView,
+    LeadCreateView,
+    LeadDetailView,
+    LeadUpdateView,
+    LeadDeleteView,
+)
+
+app_name = "leads"
+
+urlpatterns = [
+    path("", LeadListView.as_view(), name="leads-list"),
+    path("create/", LeadCreateView.as_view(), name="leads-create"),
+    path("<int:pk>/", LeadDetailView.as_view(), name="leads-detail"),
+    path("<int:pk>/edit/", LeadUpdateView.as_view(), name="leads-edit"),
+    path("<int:pk>/delete/", LeadDeleteView.as_view(), name="leads-delete"),
+]
