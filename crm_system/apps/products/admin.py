e0a3c9dc9c2e0eb422c163ff506b2366c8c242a5
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 15:30:08 +1000

    Добавил код для админки.

--- crm_system/apps/products/admin.py
+++ crm_system/apps/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
