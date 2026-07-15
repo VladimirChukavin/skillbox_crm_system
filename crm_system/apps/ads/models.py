8a49699f0525b948832515ade01ceddf88b8f979
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 15:30:48 +1000

    Добавил код для модели.

--- crm_system/apps/ads/models.py
+++ crm_system/apps/ads/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class AdCampaign(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    product = models.ForeignKey(
+        "Product",
+        on_delete=models.CASCADE,
+        related_name="ad_campaigns",
+        verbose_name="Рекламируемая услуга",
+    )
+    channel = models.CharField(max_length=255, verbose_name="Канал продвижения")
+    budget = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Бюджет")
+
+    class Meta:
+        verbose_name = "Рекламная кампания"
+        verbose_name_plural = "Рекламные кампании"
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
