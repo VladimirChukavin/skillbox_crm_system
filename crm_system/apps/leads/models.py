6d3038e08ffde45385879c9cdc1abd3361254e9d
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 15:31:17 +1000

    Добавил код для модели.

--- crm_system/apps/leads/models.py
+++ crm_system/apps/leads/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Lead(models.Model):
+    full_name = models.CharField(max_length=255, verbose_name="Ф.И.О")
+    phone = models.CharField(max_length=15, verbose_name="Телефон")
+    email = models.EmailField(verbose_name="Email")
+    ad_campaign = models.ForeignKey(
+        "AdCampaign",
+        on_delete=models.CASCADE,
+        related_name="leads",
+        verbose_name="Рекламная кампания",
+    )
+
+    class Meta:
+        verbose_name = "Потенциальный клиент"
+        verbose_name_plural = "Потенциальные клиенты"
+        ordering = ("full_name",)
+
+    def __str__(self):
+        return self.full_name
