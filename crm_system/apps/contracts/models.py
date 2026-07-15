25fcb30d4cbb88a0e73e7c8458696a614618c070
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 15:30:59 +1000

    Добавил код для модели.

--- crm_system/apps/contracts/models.py
+++ crm_system/apps/contracts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class Contract(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="Название")
+    product = models.ForeignKey(
+        "Product",
+        on_delete=models.CASCADE,
+        related_name="contracts",
+        verbose_name="Услуга",
+    )
+    document = models.FileField(
+        upload_to="contracts/documents/%Y/%m", verbose_name="Файл"
+    )
+    conclusion_date = models.DateField(verbose_name="Дата заключения")
+    duration = models.PositiveIntegerField(verbose_name="Период действия (дней)")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма")
+
+    class Meta:
+        verbose_name = "Контракт"
+        verbose_name_plural = "Контракты"
+        ordering = ("-conclusion_date",)
+
+    def __str__(self):
+        return self.name
