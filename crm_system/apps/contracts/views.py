cc9b9a9f5174e6b4143194445b79b726c395f8ef
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-15 16:32:09 +1000

    Добавил код для представлений.

--- crm_system/apps/contracts/views.py
+++ crm_system/apps/contracts/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import (
+    ListView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+    DetailView,
+)
 
-# Create your views here.
+from .models import Contract
+
+
+class ContractListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    model = Contract
+    template_name = "contracts/contracts-list.html"
+    context_object_name = "contracts"
+    permission_required = "contracts.view_contract"
+
+
+class ContractCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    model = Contract
+    fields = ["name", "product", "document", "conclusion_date", "duration", "amount"]
+    template_name = "contracts/contracts-create.html"
+    success_url = reverse_lazy("contracts:contracts-list")
+    permission_required = "contracts.add_contract"
+
+
+class ContractDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    model = Contract
+    template_name = "contracts/contracts-detail.html"
+    permission_required = "contracts.view_contract"
+
+
+class ContractUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    model = Contract
+    fields = ["name", "product", "document", "conclusion_date", "duration", "amount"]
+    template_name = "contracts/contracts-edit.html"
+    success_url = reverse_lazy("contracts:contracts-list")
+    permission_required = "contracts.change_contract"
+
+
+class ContractDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    model = Contract
+    template_name = "contracts/contracts-delete.html"
+    success_url = reverse_lazy("contracts:contracts-list")
+    permission_required = "contracts.delete_contract"
