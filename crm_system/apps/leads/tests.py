d3100d11e42599121125e3b95b95e49f44d0d0c0
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-16 14:43:35 +1000

    Добавил код для тестов.

--- crm_system/apps/leads/tests.py
+++ crm_system/apps/leads/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.urls import reverse
+
+from .models import Lead
+
+
+@pytest.mark.django_db
+def test_lead_list_view(operator_client, lead: Lead) -> None:
+    response = operator_client.get(reverse("leads:leads-list"))
+    assert response.status_code == 200
+    assert lead.full_name.encode() in response.content
+
+
+@pytest.mark.django_db
+def test_lead_create_view(operator_client, ad_campaign) -> None:
+    response = operator_client.post(
+        reverse("leads:lead-create"),
+        data={
+            "full_name": "Василий Пупкин",
+            "phone": "+79111111111",
+            "email": "pupkin@test.com",
+            "ad_campaign": ad_campaign.pk,
+        },
+    )
+    assert response.status_code == 302
+    assert Lead.objects.filter(full_name="Василий Пупкин").exists()
+
+
+@pytest.mark.django_db
+def test_lead_detail_view(operator_client, lead: Lead) -> None:
+    response = operator_client.get(
+        reverse("leads:leads-detail", kwargs={"pk": lead.pk}),
+    )
+    assert response.status_code == 200
