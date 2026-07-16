295cbc2caffb7df584cab7ced65975326909e227
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-16 18:11:57 +1000

    Поменял порядок путей.

--- crm_system/apps/ads/urls.py
+++ crm_system/apps/ads/urls.py
@@ -14,8 +14,8 @@ app_name = "ads"
 urlpatterns = [
     path("", AdCampaignListView.as_view(), name="ads-list"),
     path("create/", AdCampaignCreateView.as_view(), name="ads-create"),
+    path("statistic/", AdCampaignStatisticView.as_view(), name="ads-statistic"),
     path("<int:pk>/", AdCampaignDetailView.as_view(), name="ads-detail"),
     path("<int:pk>/edit/", AdCampaignUpdateView.as_view(), name="ads-edit"),
     path("<int:pk>/delete/", AdCampaignDeleteView.as_view(), name="ads-delete"),
-    path("statistic/", AdCampaignStatisticView.as_view(), name="ads-statistic"),
 ]
