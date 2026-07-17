66d4df13b1b5da73960fd53bbe46cf8fb5c09334
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-17 20:43:13 +1000

    Создал папку views.

---
+++ crm_system/apps/products/views/__init__.py
@@ -0,0 +1,5 @@
+from .product_list_view import ProductListView
+from .product_create_view import ProductCreateView
+from .product_detail_view import ProductDetailView
+from .product_update_view import ProductUpdateView
+from .product_delete_view import ProductDeleteView
