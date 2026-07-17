80436800c261de53899d8a917ddf6dbe003b4818
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-17 14:59:26 +1000

    Добавил путь при выходе пользователя из системы.

--- crm_system/config/urls.py
+++ crm_system/config/urls.py
@@ -30,7 +30,11 @@ urlpatterns = [
         LoginView.as_view(template_name="registration/login.html"),
         name="login",
     ),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    path(
+        "logout/",
+        LogoutView.as_view(template_name="registration/logout.html"),
+        name="logout",
+    ),
     path("ads/", include("apps.ads.urls")),
     path("contracts/", include("apps.contracts.urls")),
     path("customers/", include("apps.customers.urls")),
