17268ef8d75ee61fd38542fd194afdfbb05a4798
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-17 13:22:19 +1000

    Добавил настройки для админки.

--- crm_system/apps/leads/admin.py
+++ crm_system/apps/leads/admin.py
@@ -5,7 +5,9 @@ from .models import Lead
 
 @admin.register(Lead)
 class LeadAdmin(admin.ModelAdmin):
-    list_display = ("id", "full_name", "phone", "email", "ad_campaign")
-    list_display_links = ("full_name",)
-    search_fields = ("full_name", "phone", "email")
-    list_filter = ("ad_campaign",)
+    list_display: tuple[str, ...] = ("id", "full_name", "phone", "email", "ad_campaign")
+    list_display_links: tuple[str, ...] = ("full_name",)
+    search_fields: tuple[str, ...] = ("full_name", "phone", "email")
+    list_filter: tuple[str, ...] = ("ad_campaign", "ad_campaign__product")
+    ordering: tuple[str, ...] = ("full_name",)
+    autocomplete_fields: tuple[str, ...] = ("ad_campaign",)
