c4be6f7367f1a324acc252561f72e328820bec38
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-17 13:26:33 +1000

    Добавил метод в представление списка потенциальных клиентов, который показывает клиентов еще не ставших активными.

--- crm_system/apps/leads/views.py
+++ crm_system/apps/leads/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.db.models import QuerySet
 from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
@@ -16,6 +17,9 @@ class LeadListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
     template_name = "leads/leads-list.html"
     context_object_name = "leads"
     permission_required = "leads.view_lead"
+
+    def get_queryset(self) -> QuerySet[Lead]:
+        return Lead.objects.filter(customer__isnull=True)
 
 
 class LeadCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
