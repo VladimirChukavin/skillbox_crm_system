5fa6aa21c8525be269568cf8d7674fa340261752
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-17 14:57:56 +1000

    Добавил метод в админку списка потенциальных клиентов, который показывает клиентов еще не ставших активными.

--- crm_system/apps/leads/admin.py
+++ crm_system/apps/leads/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.db.models import QuerySet
 
 from .models import Lead
 
@@ -11,3 +12,6 @@ class LeadAdmin(admin.ModelAdmin):
     list_filter: tuple[str, ...] = ("ad_campaign", "ad_campaign__product")
     ordering: tuple[str, ...] = ("full_name",)
     autocomplete_fields: tuple[str, ...] = ("ad_campaign",)
+
+    def get_queryset(self, request) -> QuerySet[Lead]:
+        return super().get_queryset(request).filter(customer__isnull=True)
