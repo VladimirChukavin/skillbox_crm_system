bf70bf5ace1b88c9d81ac850911c58dc2f4e094e
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-17 20:43:07 +1000

    Создал папку views.

---
+++ crm_system/apps/leads/views/__init__.py
@@ -0,0 +1,5 @@
+from .lead_list_view import LeadListView
+from .lead_create_view import LeadCreateView
+from .lead_detail_view import LeadDetailView
+from .lead_update_view import LeadUpdateView
+from .lead_delete_view import LeadDeleteView
