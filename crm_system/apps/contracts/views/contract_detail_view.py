7002a6a8a4333fbfa6b9ff5ab0a019d0fe8bc779
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:47:40 +1000

    Добавил описание для класса.

--- crm_system/apps/contracts/views/contract_detail_view.py
+++ crm_system/apps/contracts/views/contract_detail_view.py
@@ -1,3 +1,5 @@
+"""Представление для отображения детальной информации о контракте."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import DetailView
 
@@ -5,6 +7,16 @@ from apps.contracts.models import Contract
 
 
 class ContractDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    """Представление для отображения детальной информации о контракте.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Contract]
+    :ivar template_name: Путь к HTML-шаблону детальной страницы.
+    :vartype template_name: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Contract
     template_name = "contracts/contracts-detail.html"
     permission_required = "contracts.view_contract"
