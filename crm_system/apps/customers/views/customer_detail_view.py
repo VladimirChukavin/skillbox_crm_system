8b0dc5bfa98a5c7ee00102f2d15a1d91c537035b
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 00:35:54 +1000

    Добавил описание для класса.

--- crm_system/apps/customers/views/customer_detail_view.py
+++ crm_system/apps/customers/views/customer_detail_view.py
@@ -1,3 +1,5 @@
+"""Представление для отображения детальной информации об активном клиенте."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import DetailView
 
@@ -5,6 +7,16 @@ from apps.customers.models import Customer
 
 
 class CustomerDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    """Представление для отображения детальной информации об активном клиенте.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Customer]
+    :ivar template_name: Путь к HTML-шаблону детальной страницы.
+    :vartype template_name: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Customer
     template_name = "customers/customers-detail.html"
     permission_required = "customers.view_customer"
