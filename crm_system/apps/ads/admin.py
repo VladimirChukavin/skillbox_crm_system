afae5288bfd3269cbc7224352028af53b64949c8
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 22:57:56 +1000

    Добавил описание для класса.

--- crm_system/apps/ads/admin.py
+++ crm_system/apps/ads/admin.py
@@ -1,3 +1,5 @@
+"""Настройки административной панели для рекламных кампаний."""
+
 from django.contrib import admin
 
 from .models import AdCampaign
@@ -5,6 +7,22 @@ from .models import AdCampaign
 
 @admin.register(AdCampaign)
 class AdCampaignAdmin(admin.ModelAdmin):
+    """Конфигурация отображения рекламных кампаний в админ-панели Django.
+
+    :ivar list_display: Поля, отображаемые в списке записей.
+    :vartype list_display: tuple[str, ...]
+    :ivar list_display_links: Поля, являющиеся ссылками на форму редактирования.
+    :vartype list_display_links: tuple[str, ...]
+    :ivar search_fields: Поля, доступные для поиска.
+    :vartype search_fields: tuple[str, ...]
+    :ivar list_filter: Поля для боковой панели фильтрации.
+    :vartype list_filter: tuple[str, ...]
+    :ivar ordering: Порядок сортировки записей по умолчанию.
+    :vartype ordering: tuple[str, ...]
+    :ivar autocomplete_fields: Поля с автодополнением.
+    :vartype autocomplete_fields: tuple[str, ...]
+    """
+
     list_display: tuple[str, ...] = ("id", "name", "product", "channel", "budget")
     list_display_links: tuple[str, ...] = ("id", "name")
     search_fields: tuple[str, ...] = ("id", "name", "channel", "budget")
