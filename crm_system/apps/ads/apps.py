50be676bc445bfe5bf5a2cc5aa854cd7eecc2fb7
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:20:03 +1000

    Добавил описание для класса.

--- crm_system/apps/ads/apps.py
+++ crm_system/apps/ads/apps.py
@@ -1,6 +1,16 @@
+"""Конфигурация приложения «Рекламные кампании»."""
+
 from django.apps import AppConfig
 
 
 class AdsConfig(AppConfig):
+    """Конфигурация Django-приложения для управления рекламными кампаниями.
+
+    :ivar name: Полный Python-путь к пакету приложения.
+    :vartype name: str
+    :ivar verbose_name: Читаемое название приложения.
+    :vartype verbose_name: str
+    """
+
     name = "apps.ads"
     verbose_name = "Рекламные кампании"
