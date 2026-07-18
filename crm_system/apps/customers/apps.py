ab5bc6307091ce66dee5cf4559100ebf067e28c5
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:20:26 +1000

    Добавил описание для класса.

--- crm_system/apps/customers/apps.py
+++ crm_system/apps/customers/apps.py
@@ -1,6 +1,16 @@
+"""Конфигурация приложения «Активные клиенты»."""
+
 from django.apps import AppConfig
 
 
 class CustomersConfig(AppConfig):
+    """Конфигурация Django-приложения для управления активными клиентами.
+
+    :ivar name: Полный Python-путь к пакету приложения.
+    :vartype name: str
+    :ivar verbose_name: Читаемое название приложения.
+    :vartype verbose_name: str
+    """
+
     name = "apps.customers"
     verbose_name = "Активные клиенты"
