84cec84d9eabd0236ba85b9dcc5dc40952a658ed
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:20:39 +1000

    Добавил описание для класса.

--- crm_system/apps/products/apps.py
+++ crm_system/apps/products/apps.py
@@ -1,6 +1,16 @@
+"""Конфигурация приложения «Услуги»."""
+
 from django.apps import AppConfig
 
 
 class ProductsConfig(AppConfig):
+    """Конфигурация Django-приложения для управления услугами.
+
+    :ivar name: Полный Python-путь к пакету приложения.
+    :vartype name: str
+    :ivar verbose_name: Читаемое название приложения.
+    :vartype verbose_name: str
+    """
+
     name = "apps.products"
     verbose_name = "Услуги"
