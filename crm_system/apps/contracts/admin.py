1a1ebde0ed3d1e3f086bc4dee9652ad5926dfcb6
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:47:19 +1000

    Добавил описание для класса.

--- crm_system/apps/contracts/admin.py
+++ crm_system/apps/contracts/admin.py
@@ -1,3 +1,5 @@
+"""Настройки административной панели для контрактов."""
+
 from django.contrib import admin
 
 from .models import Contract
@@ -5,6 +7,24 @@ from .models import Contract
 
 @admin.register(Contract)
 class ContractAdmin(admin.ModelAdmin):
+    """Конфигурация отображения контрактов в админ-панели Django.
+
+    :ivar list_display: Поля, отображаемые в списке записей.
+    :vartype list_display: tuple[str, ...]
+    :ivar list_display_links: Поля, являющиеся ссылками на форму редактирования.
+    :vartype list_display_links: tuple[str, ...]
+    :ivar search_fields: Поля, доступные для поиска.
+    :vartype search_fields: tuple[str, ...]
+    :ivar list_filter: Поля для боковой панели фильтрации.
+    :vartype list_filter: tuple[str, ...]
+    :ivar ordering: Порядок сортировки записей по умолчанию.
+    :vartype ordering: tuple[str, ...]
+    :ivar autocomplete_fields: Поля с автодополнением.
+    :vartype autocomplete_fields: tuple[str, ...]
+    :ivar readonly_fields: Поля, доступные только для чтения.
+    :vartype readonly_fields: tuple[str, ...]
+    """
+
     list_display: tuple[str, ...] = (
         "id",
         "name",
