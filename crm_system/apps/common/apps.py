873975d5e698defcad4801f6212eeda014f1c9d9
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:20:11 +1000

    Добавил описание для класса.

--- crm_system/apps/common/apps.py
+++ crm_system/apps/common/apps.py
@@ -1,5 +1,13 @@
+"""Конфигурация приложения «Общие настройки»."""
+
 from django.apps import AppConfig
 
 
 class CommonConfig(AppConfig):
+    """Конфигурация Django-приложения для общих настроек.
+
+    :ivar name: Полный Python-путь к пакету приложения.
+    :vartype name: str
+    """
+
     name = "apps.common"
