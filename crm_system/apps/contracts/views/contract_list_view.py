98293e0a3ade0235ca7f36cf4d6aea9683d1dc8d
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:47:46 +1000

    Добавил описание для класса.

--- crm_system/apps/contracts/views/contract_list_view.py
+++ crm_system/apps/contracts/views/contract_list_view.py
@@ -1,3 +1,5 @@
+"""Представление для отображения списка контрактов."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import ListView
 
@@ -5,6 +7,18 @@ from apps.contracts.models import Contract
 
 
 class ContractListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    """Представление для отображения списка всех контрактов.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Contract]
+    :ivar template_name: Путь к HTML-шаблону страницы списка.
+    :vartype template_name: str
+    :ivar context_object_name: Имя переменной контекста для списка объектов.
+    :vartype context_object_name: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Contract
     template_name = "contracts/contracts-list.html"
     context_object_name = "contracts"
