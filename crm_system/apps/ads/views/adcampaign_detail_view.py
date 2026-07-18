bbc50f1d2139df24312da4c7c9b27e572d0d8d4e
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 22:57:20 +1000

    Добавил описание для класса.

--- crm_system/apps/ads/views/adcampaign_detail_view.py
+++ crm_system/apps/ads/views/adcampaign_detail_view.py
@@ -1,3 +1,5 @@
+"""Представление для отображения детальной информации о рекламной кампании."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import DetailView
 
@@ -5,6 +7,16 @@ from apps.ads.models import AdCampaign
 
 
 class AdCampaignDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    """Представление для отображения детальной информации о рекламной кампании.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[AdCampaign]
+    :ivar template_name: Путь к HTML-шаблону детальной страницы.
+    :vartype template_name: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = AdCampaign
     template_name = "ads/ads-detail.html"
     permission_required = "ads.view_adcampaign"
