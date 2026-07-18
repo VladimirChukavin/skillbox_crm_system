c530ee41401429e6b7000dc1770803a19f348bd8
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:48:53 +1000

    Добавил описание для класса, метода.

--- crm_system/apps/contracts/models.py
+++ crm_system/apps/contracts/models.py
@@ -1,7 +1,25 @@
+"""Модель контракта."""
+
 from django.db import models
 
 
 class Contract(models.Model):
+    """Модель для хранения данных о контракте на предоставление услуги.
+
+    :ivar name: Название контракта.
+    :vartype name: str
+    :ivar product: Предоставляемая услуга (связь с моделью Product).
+    :vartype product: Product
+    :ivar document: Файл с документом контракта.
+    :vartype document: django.db.models.fields.files.FieldFile
+    :ivar conclusion_date: Дата заключения контракта.
+    :vartype conclusion_date: datetime.date
+    :ivar duration: Период действия контракта в днях.
+    :vartype duration: int
+    :ivar amount: Сумма контракта.
+    :vartype amount: decimal.Decimal
+    """
+
     name = models.CharField(max_length=255, unique=True, verbose_name="Название")
     product = models.ForeignKey(
         "products.Product",
@@ -21,5 +39,10 @@ class Contract(models.Model):
         verbose_name_plural = "Контракты"
         ordering = ("-conclusion_date",)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Возвращает строковое представление контракта.
+
+        :returns: Название контракта.
+        :rtype: str
+        """
         return self.name
