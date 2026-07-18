8b2e897694fbb8caaf71770f1aeb87902dc2b7f2
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:47:52 +1000

    Добавил описание для класса.

--- crm_system/apps/contracts/views/contract_update_view.py
+++ crm_system/apps/contracts/views/contract_update_view.py
@@ -1,3 +1,5 @@
+"""Представление для редактирования контракта."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
@@ -6,6 +8,20 @@ from apps.contracts.models import Contract
 
 
 class ContractUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    """Представление для редактирования существующего контракта.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Contract]
+    :ivar fields: Список полей формы, доступных для редактирования.
+    :vartype fields: list[str]
+    :ivar template_name: Путь к HTML-шаблону страницы редактирования.
+    :vartype template_name: str
+    :ivar success_url: URL для перенаправления после успешного обновления.
+    :vartype success_url: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Contract
     fields = ["name", "product", "document", "conclusion_date", "duration", "amount"]
     template_name = "contracts/contracts-edit.html"
