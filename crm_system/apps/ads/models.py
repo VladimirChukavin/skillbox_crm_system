38e6a193ca696f7df14b2b630940f7bf52ac2a3f
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 22:58:12 +1000

    Добавил описание для класса.

--- crm_system/apps/ads/models.py
+++ crm_system/apps/ads/models.py
@@ -1,7 +1,21 @@
+"""Модель рекламной кампании."""
+
 from django.db import models
 
 
 class AdCampaign(models.Model):
+    """Модель для хранения данных о рекламной кампании.
+
+    :ivar name: Название рекламной кампании.
+    :vartype name: str
+    :ivar product: Рекламируемая услуга (связь с моделью Product).
+    :vartype product: Product
+    :ivar channel: Канал продвижения.
+    :vartype channel: str
+    :ivar budget: Бюджет на рекламу.
+    :vartype budget: decimal.Decimal
+    """
+
     name = models.CharField(max_length=255, unique=True, verbose_name="Название")
     product = models.ForeignKey(
         "products.Product",
@@ -17,5 +31,10 @@ class AdCampaign(models.Model):
         verbose_name_plural = "Рекламные кампании"
         ordering = ("name",)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Возвращает строковое представление рекламной кампании.
+
+        :returns: Название рекламной кампании.
+        :rtype: str
+        """
         return self.name
