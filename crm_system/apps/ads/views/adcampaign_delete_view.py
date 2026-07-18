457d5ab0abd11ed71e53d4a311d4de648df7bc72
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 22:57:14 +1000

    Добавил описание для класса.

--- crm_system/apps/ads/views/adcampaign_delete_view.py
+++ crm_system/apps/ads/views/adcampaign_delete_view.py
@@ -1,3 +1,5 @@
+"""Представление для удаления рекламной кампании."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView
@@ -6,6 +8,18 @@ from apps.ads.models import AdCampaign
 
 
 class AdCampaignDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    """Представление для удаления существующей рекламной кампании.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[AdCampaign]
+    :ivar template_name: Путь к HTML-шаблону страницы подтверждения удаления.
+    :vartype template_name: str
+    :ivar success_url: URL для перенаправления после успешного удаления.
+    :vartype success_url: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = AdCampaign
     template_name = "ads/ads-delete.html"
     success_url = reverse_lazy("ads:ads-list")
