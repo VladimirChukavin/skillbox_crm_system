ef1e94676c4145b29dc803f10ad80e49ef46734a
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:20:20 +1000

    Добавил описание для класса.

--- crm_system/apps/contracts/apps.py
+++ crm_system/apps/contracts/apps.py
@@ -1,6 +1,16 @@
+"""Конфигурация приложения «Контракты»."""
+
 from django.apps import AppConfig
 
 
 class ContractsConfig(AppConfig):
+    """Конфигурация Django-приложения для управления контрактами.
+
+    :ivar name: Полный Python-путь к пакету приложения.
+    :vartype name: str
+    :ivar verbose_name: Читаемое название приложения.
+    :vartype verbose_name: str
+    """
+
     name = "apps.contracts"
     verbose_name = "Контракты"
