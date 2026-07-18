1af2ca9a220fda7276aeede4711ebb3287d72132
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:47:35 +1000

    Добавил описание для класса.

--- crm_system/apps/contracts/views/contract_delete_view.py
+++ crm_system/apps/contracts/views/contract_delete_view.py
@@ -1,3 +1,5 @@
+"""Представление для удаления контракта."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView
@@ -6,6 +8,18 @@ from apps.contracts.models import Contract
 
 
 class ContractDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    """Представление для удаления существующего контракта.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Contract]
+    :ivar template_name: Путь к HTML-шаблону страницы подтверждения удаления.
+    :vartype template_name: str
+    :ivar success_url: URL для перенаправления после успешного удаления.
+    :vartype success_url: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Contract
     template_name = "contracts/contracts-delete.html"
     success_url = reverse_lazy("contracts:contracts-list")
