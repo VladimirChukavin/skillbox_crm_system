b37ce159c433b215e0076cc59b1c276b436ea33e
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-18 23:20:31 +1000

    Добавил описание для класса.

--- crm_system/apps/leads/apps.py
+++ crm_system/apps/leads/apps.py
@@ -1,6 +1,16 @@
+"""Конфигурация приложения «Потенциальные клиенты»."""
+
 from django.apps import AppConfig
 
 
 class LeadsConfig(AppConfig):
+    """Конфигурация Django-приложения для управления потенциальными клиентами.
+
+    :ivar name: Полный Python-путь к пакету приложения.
+    :vartype name: str
+    :ivar verbose_name: Читаемое название приложения.
+    :vartype verbose_name: str
+    """
+
     name = "apps.leads"
     verbose_name = "Потенциальные клиенты"
