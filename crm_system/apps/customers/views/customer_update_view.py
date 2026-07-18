046dba8a559a8c680875224e34105c6b2b128887
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 00:36:11 +1000

    Добавил описание для класса.

--- crm_system/apps/customers/views/customer_update_view.py
+++ crm_system/apps/customers/views/customer_update_view.py
@@ -1,3 +1,5 @@
+"""Представление для редактирования активного клиента."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
@@ -6,6 +8,20 @@ from apps.customers.models import Customer
 
 
 class CustomerUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    """Представление для редактирования существующего активного клиента.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Customer]
+    :ivar fields: Список полей формы, доступных для редактирования.
+    :vartype fields: list[str]
+    :ivar template_name: Путь к HTML-шаблону страницы редактирования.
+    :vartype template_name: str
+    :ivar success_url: URL для перенаправления после успешного обновления.
+    :vartype success_url: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Customer
     fields = ["lead", "contract"]
     template_name = "customers/customers-edit.html"
