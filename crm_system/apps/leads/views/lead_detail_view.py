63e796bfd16479fd4fd6e6b91cca3cc78156438c
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:40:35 +1000

    Добавил описание для класса.

--- crm_system/apps/leads/views/lead_detail_view.py
+++ crm_system/apps/leads/views/lead_detail_view.py
@@ -1,3 +1,5 @@
+"""Представление для отображения детальной информации о потенциальном клиенте."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import DetailView
 
@@ -5,6 +7,16 @@ from apps.leads.models import Lead
 
 
 class LeadDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    """Представление для отображения детальной информации о потенциальном клиенте.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Lead]
+    :ivar template_name: Путь к HTML-шаблону детальной страницы.
+    :vartype template_name: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Lead
     template_name = "leads/leads-detail.html"
     permission_required = "leads.view_lead"
