259988ab707c4d5896f8c638732533ba83ba4dd0
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:40:55 +1000

    Добавил описание для класса.

--- crm_system/apps/leads/views/lead_update_view.py
+++ crm_system/apps/leads/views/lead_update_view.py
@@ -1,3 +1,5 @@
+"""Представление для редактирования потенциального клиента."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
@@ -6,6 +8,20 @@ from apps.leads.models import Lead
 
 
 class LeadUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    """Представление для редактирования существующего потенциального клиента.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Lead]
+    :ivar fields: Список полей формы, доступных для редактирования.
+    :vartype fields: list[str]
+    :ivar template_name: Путь к HTML-шаблону страницы редактирования.
+    :vartype template_name: str
+    :ivar success_url: URL для перенаправления после успешного обновления.
+    :vartype success_url: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Lead
     fields = ["full_name", "phone", "email", "ad_campaign"]
     template_name = "leads/leads-edit.html"
