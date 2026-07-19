46595fe69e363b0538e34d21eeb23cdfa9aa84d3
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:41:37 +1000

    Добавил описание для класса.

--- crm_system/apps/products/views/product_detail_view.py
+++ crm_system/apps/products/views/product_detail_view.py
@@ -1,3 +1,5 @@
+"""Представление для отображения детальной информации об услуге."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import DetailView
 
@@ -5,6 +7,16 @@ from apps.products.models import Product
 
 
 class ProductDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    """Представление для отображения детальной информации об услуге.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Product]
+    :ivar template_name: Путь к HTML-шаблону детальной страницы.
+    :vartype template_name: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Product
     template_name = "products/products-detail.html"
     permission_required = "products.view_product"
