f2ce809162598fc46a55f1c599fc91dd8de9279f
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 18:55:04 +1000

    Исправил тип переменных.

--- crm_system/apps/ads/admin.py
+++ crm_system/apps/ads/admin.py
@@ -1,5 +1,7 @@
 """Настройки административной панели для рекламных кампаний."""
 
+from typing import ClassVar
+
 from django.contrib import admin
 
 from .models import AdCampaign
@@ -25,7 +27,7 @@ class AdCampaignAdmin(admin.ModelAdmin):
 
     list_display: tuple[str, ...] = ("id", "name", "product", "channel", "budget")
     list_display_links: tuple[str, ...] = ("id", "name")
-    search_fields: tuple[str, ...] = ("id", "name", "channel", "budget")
-    list_filter: tuple[str, ...] = ("product", "channel", "budget")
-    ordering: tuple[str, ...] = ("name",)
-    autocomplete_fields: tuple[str, ...] = ("product",)
+    search_fields: ClassVar[tuple[str, ...]] = ("id", "name", "channel", "budget")
+    list_filter: ClassVar[tuple[str, ...]] = ("product", "channel", "budget")
+    ordering: ClassVar[tuple[str, ...]] = ("name",)
+    autocomplete_fields: ClassVar[tuple[str, ...]] = ("product",)
