0f8ca311015d792e50613bd8b41fd6338d7c1982
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:41:07 +1000

    Добавил описание для класса.

--- crm_system/apps/leads/models.py
+++ crm_system/apps/leads/models.py
@@ -1,7 +1,21 @@
+"""Модель потенциального клиента."""
+
 from django.db import models
 
 
 class Lead(models.Model):
+    """Модель для хранения данных о потенциальном клиенте.
+
+    :ivar full_name: Ф.И.О потенциального клиента.
+    :vartype full_name: str
+    :ivar phone: Номер телефона.
+    :vartype phone: str
+    :ivar email: Электронная почта.
+    :vartype email: str
+    :ivar ad_campaign: Рекламная кампания, из которой пришёл лид (связь с моделью AdCampaign).
+    :vartype ad_campaign: AdCampaign
+    """
+
     full_name = models.CharField(max_length=255, verbose_name="Ф.И.О")
     phone = models.CharField(max_length=15, verbose_name="Телефон")
     email = models.EmailField(verbose_name="Email")
@@ -17,5 +31,10 @@ class Lead(models.Model):
         verbose_name_plural = "Потенциальные клиенты"
         ordering = ("full_name",)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Возвращает строковое представление потенциального клиента.
+
+        :returns: Ф.И.О потенциального клиента.
+        :rtype: str
+        """
         return self.full_name
