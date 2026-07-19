77353a26c85c49927a41fda7beaa294a45f3dd98
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:40:23 +1000

    Добавил описание для класса.

--- crm_system/apps/leads/views/lead_create_view.py
+++ crm_system/apps/leads/views/lead_create_view.py
@@ -1,3 +1,5 @@
+"""Представление для создания потенциального клиента."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
@@ -6,6 +8,20 @@ from apps.leads.models import Lead
 
 
 class LeadCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    """Представление для создания нового потенциального клиента.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Lead]
+    :ivar fields: Список полей формы, доступных для заполнения.
+    :vartype fields: list[str]
+    :ivar template_name: Путь к HTML-шаблону страницы создания.
+    :vartype template_name: str
+    :ivar success_url: URL для перенаправления после успешного создания.
+    :vartype success_url: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Lead
     fields = ["full_name", "phone", "email", "ad_campaign"]
     template_name = "leads/leads-create.html"
