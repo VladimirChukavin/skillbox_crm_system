02c5d286d54da3f773921ccc8bc684f35759b26c
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:40:16 +1000

    Добавил описание для класса.

--- crm_system/apps/products/admin.py
+++ crm_system/apps/products/admin.py
@@ -1,3 +1,5 @@
+"""Настройки административной панели для услуг."""
+
 from django.contrib import admin
 
 from .models import Product
@@ -5,6 +7,18 @@ from .models import Product
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
+    """Конфигурация отображения услуг в админ-панели Django.
+
+    :ivar list_display: Поля, отображаемые в списке записей.
+    :vartype list_display: tuple[str, ...]
+    :ivar list_display_links: Поля, являющиеся ссылками на форму редактирования.
+    :vartype list_display_links: tuple[str, ...]
+    :ivar search_fields: Поля, доступные для поиска.
+    :vartype search_fields: tuple[str, ...]
+    :ivar ordering: Порядок сортировки записей по умолчанию.
+    :vartype ordering: tuple[str, ...]
+    """
+
     list_display: tuple[str, ...] = ("id", "name", "price")
     list_display_links: tuple[str, ...] = ("id", "name")
     search_fields: tuple[str, ...] = ("name",)
