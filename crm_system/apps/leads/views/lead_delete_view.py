e7e55b44a90858c486fe15efdfd53823c6b77973
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:40:29 +1000

    Добавил описание для класса.

--- crm_system/apps/leads/views/lead_delete_view.py
+++ crm_system/apps/leads/views/lead_delete_view.py
@@ -1,3 +1,5 @@
+"""Представление для удаления потенциального клиента."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView
@@ -6,6 +8,18 @@ from apps.leads.models import Lead
 
 
 class LeadDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    """Представление для удаления существующего потенциального клиента.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Lead]
+    :ivar template_name: Путь к HTML-шаблону страницы подтверждения удаления.
+    :vartype template_name: str
+    :ivar success_url: URL для перенаправления после успешного удаления.
+    :vartype success_url: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Lead
     template_name = "leads/leads-delete.html"
     success_url = reverse_lazy("leads:leads-list")
