e1c6a62fdb5ade8eda90938e59014eba3ecae8db
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:41:47 +1000

    Добавил описание для класса.

--- crm_system/apps/products/views/product_update_view.py
+++ crm_system/apps/products/views/product_update_view.py
@@ -1,3 +1,5 @@
+"""Представление для редактирования услуги."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import UpdateView
@@ -6,6 +8,20 @@ from apps.products.models import Product
 
 
 class ProductUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    """Представление для редактирования существующей услуги.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Product]
+    :ivar fields: Список полей формы, доступных для редактирования.
+    :vartype fields: list[str]
+    :ivar template_name: Путь к HTML-шаблону страницы редактирования.
+    :vartype template_name: str
+    :ivar success_url: URL для перенаправления после успешного обновления.
+    :vartype success_url: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Product
     fields = ["name", "description", "price"]
     template_name = "products/products-edit.html"
