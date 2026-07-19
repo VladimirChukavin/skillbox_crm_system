2f8c8188ecfb84924962c263ed072cd324b29728
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:41:17 +1000

    Добавил описание для класса.

--- crm_system/apps/products/models.py
+++ crm_system/apps/products/models.py
@@ -1,7 +1,19 @@
+"""Модель услуги."""
+
 from django.db import models
 
 
 class Product(models.Model):
+    """Модель для хранения данных об услуге.
+
+    :ivar name: Название услуги.
+    :vartype name: str
+    :ivar description: Описание услуги.
+    :vartype description: str
+    :ivar price: Стоимость услуги.
+    :vartype price: decimal.Decimal
+    """
+
     name = models.CharField(max_length=255, unique=True, verbose_name="Название")
     description = models.TextField(blank=True, default="", verbose_name="Описание")
     price = models.DecimalField(
@@ -13,5 +25,10 @@ class Product(models.Model):
         verbose_name_plural = "Услуги"
         ordering = ("name",)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Возвращает строковое представление услуги.
+
+        :returns: Название услуги.
+        :rtype: str
+        """
         return self.name
