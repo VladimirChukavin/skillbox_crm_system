7031a10a32b0539964185ff541cb8bc1bd3ed18d
VladimirChukavin <dartmorgot@yandex.ru>
2026-07-19 16:41:30 +1000

    Добавил описание для класса.

--- crm_system/apps/products/views/product_delete_view.py
+++ crm_system/apps/products/views/product_delete_view.py
@@ -1,3 +1,5 @@
+"""Представление для удаления услуги."""
+
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import DeleteView
@@ -6,6 +8,18 @@ from apps.products.models import Product
 
 
 class ProductDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    """Представление для удаления существующей услуги.
+
+    :ivar model: Модель, связанная с представлением.
+    :vartype model: type[Product]
+    :ivar template_name: Путь к HTML-шаблону страницы подтверждения удаления.
+    :vartype template_name: str
+    :ivar success_url: URL для перенаправления после успешного удаления.
+    :vartype success_url: str
+    :ivar permission_required: Требуемое разрешение для доступа к представлению.
+    :vartype permission_required: str
+    """
+
     model = Product
     template_name = "products/products-delete.html"
     success_url = reverse_lazy("products:products-list")
